3bb205c91142ccc5dbd45370c8377aadf75ed06f
Valerie Sparks <valeriesparks@Valeries-MacBook-Pro.local>
2014-02-20 20:11:06 -0500

    20_questions

---
+++ 20_questions/homework feb 5.rb
@@ -0,0 +1,10 @@
+
+questions_and_answers = {}
+questions_and_answers['barack is the prez'] = 'y'
+questions_and_answers['robby is the prez'] = 'n'
+
+puts questions_and_answers
+
+questions_and_answers.each do | question, answer |
+	
+end
